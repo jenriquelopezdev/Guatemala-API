3ba62b3d22ac7c3560fa25c604d6798b435efa7a
Enrique Lopez <elopez@digitalgeko.com>
2020-07-22 17:23:14 -0600

    login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -24,4 +24,21 @@ class AuthController extends Controller
 
         return response(['user' => $user, 'access_token' => $accessToken]);
     }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+
+    }
 }
